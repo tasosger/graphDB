86bf256ca84b95cf7bf35d652f1e56acf7740ea0
tasosger <tassos.gerassis@gmail.com>
2024-10-19 12:54:26 +0300

    traversal and fix

--- graphDB.cpp
+++ graphDB.cpp
@@ -1,13 +1,10 @@
 #include <fstream>
-#include <vector>
-#include <cstring>
 #include <iostream>
 
 using namespace std;
 
-const int NODE_RECORD_SIZE  = 9;
+const int NODE_RECORD_SIZE = 9;
 const int RELATIONSHIP_RECORD_SIZE = 17;
-
 
 struct NodeRecord {
     int id;
@@ -16,28 +13,26 @@ struct NodeRecord {
 
     NodeRecord(int nodeId = -1, int firstRelationship = -1, char usage = 0) {
         id = nodeId;
-        firstRelationship = firstRelationship;
+        firstRel = firstRelationship;
         inUse = usage;
     }
 };
-
 
 struct RelationshipRecord {
     int id;
     int fromNode;
     int toNode;
-    char inUse;
     int nextRel;
+    char inUse;
 
-    RelationshipRecord(int relId = -1, int from = -1 , int to= -1, int next = -1, char usage = 0 ) {
+    RelationshipRecord(int relId = -1, int from = -1, int to = -1, int next = -1, char usage = 0) {
         id = relId;
         fromNode = from;
-        toNode = next;
+        toNode = to;
         nextRel = next;
         inUse = usage;
     }
 };
-
 
 class GraphDB {
 private:
@@ -48,14 +43,14 @@ private:
 
 public:
     GraphDB(const string& nodeFilename, const string& relFilename) {
-        nodeFile.open(nodeFilename, ios::in | ios::out | ios::binary );
+        nodeFile.open(nodeFilename, ios::in | ios::out | ios::binary);
         if (!nodeFile) {
             nodeFile.open(nodeFilename, ios::out | ios::binary);
             nodeFile.close();
             nodeFile.open(nodeFilename, ios::in | ios::out | ios::binary);
         }
 
-        relFile.open(relFilename, ios::in | ios::out | ios::binary );
+        relFile.open(relFilename, ios::in | ios::out | ios::binary);
         if (!relFile) {
             relFile.open(relFilename, ios::out | ios::binary);
             relFile.close();
@@ -63,14 +58,13 @@ public:
         }
     }
 
-     ~GraphDB() {
+    ~GraphDB() {
         if (nodeFile.is_open()) nodeFile.close();
         if (relFile.is_open()) relFile.close();
     }
 
-
     int addNode() {
-        NodeRecord node(nextNodeId, -1, -1);
+        NodeRecord node(nextNodeId, -1, 1);
         int offset = nextNodeId * NODE_RECORD_SIZE;
 
         nodeFile.seekp(offset, ios::beg);
@@ -78,10 +72,8 @@ public:
         return nextNodeId++;
     }
 
-
     void addRelationship(int fromNodeId, int toNodeId) {
         int fromNodeOffset = fromNodeId * NODE_RECORD_SIZE;
-        int toNodeOffset = toNodeId * NODE_RECORD_SIZE;
         RelationshipRecord rel(nextRelId, fromNodeId, toNodeId, -1, 1);
         int relOffset = nextRelId * RELATIONSHIP_RECORD_SIZE;
         relFile.seekp(relOffset, ios::beg);
@@ -112,18 +104,47 @@ public:
         nodeFile.write(reinterpret_cast<char*>(&fromNode), sizeof(NodeRecord));
         nextRelId++;
     }
-};
 
+    void traverseFromNode(int nodeId) {
+        int nodeOffset = nodeId * NODE_RECORD_SIZE;
+        nodeFile.seekg(nodeOffset, ios::beg);
+        NodeRecord node;
+        nodeFile.read(reinterpret_cast<char*>(&node), sizeof(NodeRecord));
+
+        if (node.inUse) {
+            cout << "Traversing from Node ID: " << nodeId << endl;
+
+            int relId = node.firstRel;
+            while (relId != -1) {
+                int relOffset = relId * RELATIONSHIP_RECORD_SIZE;
+                relFile.seekg(relOffset, ios::beg);
+
+                RelationshipRecord rel;
+                relFile.read(reinterpret_cast<char*>(&rel), sizeof(RelationshipRecord));
+
+                if (rel.inUse) {
+                    cout << "Node " << nodeId << " is connected to Node " << rel.toNode << endl;
+                }
+
+                relId = rel.nextRel;
+            }
+        } else {
+            cout << "Node ID " << nodeId << " not found or not in use." << endl;
+        }
+    }
+};
 
 int main() {
     GraphDB graph("nodes.db", "relationships.db");
-    
 
     int nodeA = graph.addNode();
     int nodeB = graph.addNode();
     int nodeC = graph.addNode();
 
-    
-    graph.addRelationship(nodeA, nodeB); 
+    graph.addRelationship(nodeA, nodeB);
     graph.addRelationship(nodeA, nodeC);
+
+    graph.traverseFromNode(nodeA);
+
+    return 0;
 }
